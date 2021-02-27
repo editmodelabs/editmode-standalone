846ee831977af17dfd3c616749f089e0025f95cf
Jen <jengkarlong@gmail.com>
2021-02-27 23:18:47 +0800

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
-    filename: 'main.js',
+    filename: 'standalone.min.js',
     path: path.resolve(__dirname, 'dist'),
+    libraryTarget: "commonjs2"
   },
   devServer: {
     contentBase: './dist',
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/
+    })]
+  }
 };
